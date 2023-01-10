97b697adc26c1096a48a7c2d153053781e690c51
“Yulya <yulya_kevra@mail.ru>
2023-01-10 12:01:09 +0300

    feat: update index.file

--- src/index.js
+++ src/index.js
@@ -2,5 +2,21 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+ let newArr=[];
+if (matrix == undefined || matrix.length == 0){
+  return newArr;
 }
+  else{
+    for (let i=0; i<matrix.length; i++){
+    if (i % 2){
+      newArr.push(matrix[i].reverse())
+    }
+    else {
+      newArr.push(matrix[i])
+    }
+  
+}
+
+  return newArr.flat();
+  }
+  }
